78e150dda80a726e43c5e3e98c5dd3a3420826ac
alfinaswar <41230947+alfinaswar@users.noreply.github.com>
2023-12-29 09:36:24 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,16 +6,13 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+               
+                <div class="jumbotron text-center bg-white">
+                     <img src="{{ Storage::url('public/login-bg/home.png')}}" width="100" height="100">
+                <h3 class="text-inverse" data-step="1" data-intro="Hello world! I'm Intro.js" data-hint="Hello world! I'm Intro.js" data-hintposition="top-middle" data-position="bottom-right-aligned">Selamat Datang</h3>
+                <p class="lead m-b-20" data-step="2" data-intro="This is a simple step-by-step guide made using Intro.js" data-hint="This is a simple step-by-step guide made using Intro.js" data-hintposition="top-middle" data-position="bottom-right-aligned">Aplikasi Pengajuan Aplikasi Surat</p>
+                
+            </div>
             </div>
         </div>
  
